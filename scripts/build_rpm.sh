97a059e5bdc86d4425403d7eec9ec921d46d4764
Yurii Shestakov <yshestakov@gmail.com>
2020-06-15 23:07:22 +0300

    spdk.spec and build_rpm.sh: make single source tar
    Signed-off-by: Yurii Shestakov <yuriis@mellanox.com>

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -4,6 +4,7 @@ wd=$(dirname $0)
 wdir=$(dirname $wd)
 cd $wdir
 mkdir -p $HOME/rpmbuild/{SOURCES,RPMS,SRPMS,SPECS,BUILD,BUILDROOT}
+OUTDIR=$HOME/rpmbuild/SOURCES
 set -e
 if [ -z "$VER" ] ; then
     export VER=20.04.1
@@ -19,7 +20,7 @@ sed -e "s#scm_rev %{_rev}#scm_rev ${BUILD_NUMBER:-1}#" scripts/spdk.spec > $OUT_
 sed -i "s#%{_date}#$_date#; s#%{_sha1}#$sha1#; s#%{_branch}#$branch# " $OUT_SPEC
 
 git archive \
-    --format=tar.gz --prefix=spdk-$VER/ -o ~/rpmbuild/SOURCES/spdk-$VER.tar.gz  HEAD
+    --format=tar --prefix=spdk-$VER/ -o $OUTDIR/spdk-$VER.tar  HEAD
 # echo ***********
 # ls -l ~/rpmbuild/SOURCES/spdk-$VER.tar.gz
 # echo ***********
@@ -29,23 +30,17 @@ for MOD in $(git submodule |awk '{print $2}')
 do
   (cd $MOD;
    git archive \
-    --format=tar.gz --prefix=$MOD/ -o ~/rpmbuild/SOURCES/spdk-$MOD-$VER.tar.gz  HEAD
+    --format=tar --prefix=spdk-$VER/$MOD/ -o $OUTDIR/spdk-$MOD-$VER.tar  HEAD
   )
 done
-
+for MOD in $(git submodule |awk '{print $2}')
+do
+    tar --concatenate --file=$OUTDIR/spdk-$VER.tar $OUTDIR/spdk-$MOD-$VER.tar 
+done
+gzip $OUTDIR/spdk-$VER.tar
 # BUILD_NUMBER is an env var passed by Jenkins
 # https://stackoverflow.com/questions/16155792/using-jenkins-build-number-in-rpm-spec-file
 fakeroot  \
   rpmbuild -bs --define "dist %{nil}" $args $OUT_SPEC
 
-# Let's assume all dependencies are already installed
-# into *spdk_dev docker image
-#--
-# if [ "$UID" -eq 0 ] ; then
-#    chown 0.0  ~/rpmbuild/SOURCES/spdk-*-$VER.tar.gz
-#    # ls -l ~/rpmbuild/SOURCES/*
-#    # just a workaround for missed *-source repos:
-#    fgrep -l vault.centos /etc/yum.repos.d/*.repo |while read fn ; do mv $fn /tmp/ ; done
-#    yum-builddep -y $OUT_SPEC
-# fi
-rpmbuild -bb $args  $OUT_SPEC
+# rpmbuild -bb $args  $OUT_SPEC
