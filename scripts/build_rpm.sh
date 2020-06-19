23818ca5e561b089b43b7b2fbd8ccaec1ce1cef8
Yurii Shestakov <yshestakov@gmail.com>
2020-06-19 23:08:27 +0300

    Generate debian/changelog by build_rpm.sh
    Signed-off-by: Yurii Shestakov <yuriis@mellanox.com>

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+#
+function get_ver()
+{
+    by_tag=$(git describe HEAD --tags)
+    v_pfx=${by_tag%%-*}
+    v_num=${v_pfx#v}
+    echo $v_num
+}
+function generate_changelog()
+{
+    today=$(date +"%a, %d %b %Y %T %z")
+    mkdir -p spdk-$VER/debian/
+    mkdir -p spdk-$VER/scripts/debian/
+
+    for FN in debian/changelog scripts/debian/changelog ; do
+      sed -e "s/@PACKAGE_VERSION@/$VER/" -e "s/@PACKAGE_REVISION@/${BUILD_NUMBER-1}/" \
+          -e 's/@PACKAGE_BUGREPORT@/support@mellanox.com/' -e "s/@BUILD_DATE_CHANGELOG@/$today/" \
+          $FN.in > spdk-$VER/$FN
+    done
+}
 args="$@"
 wd=$(dirname $0)
 wdir=$(dirname $wd)
@@ -7,7 +27,11 @@ mkdir -p $HOME/rpmbuild/{SOURCES,RPMS,SRPMS,SPECS,BUILD,BUILDROOT}
 OUTDIR=$HOME/rpmbuild/SOURCES
 set -e
 if [ -z "$VER" ] ; then
-    export VER=20.04.1
+    VER=$(get_ver)
+    if [ -z "$VER" ] ; then
+        VER=20.04.1
+    fi
+    export VER
 fi
 branch=$(git rev-parse --abbrev-ref HEAD)
 sha1=$(git rev-parse HEAD |cut -c -8)
@@ -21,6 +45,9 @@ sed -i "s#%{_date}#$_date#; s#%{_sha1}#$sha1#; s#%{_branch}#$branch# " $OUT_SPEC
 
 git archive \
     --format=tar --prefix=spdk-$VER/ -o $OUTDIR/spdk-$VER.tar  HEAD
+generate_changelog
+tar -uf  $OUTDIR/spdk-$VER.tar spdk-$VER/debian/changelog spdk-$VER/scripts/debian/changelog
+rm -rf spdk-$VER/
 # echo ***********
 # ls -l ~/rpmbuild/SOURCES/spdk-$VER.tar.gz
 # echo ***********
@@ -37,10 +64,10 @@ for MOD in $(git submodule |awk '{print $2}')
 do
     tar --concatenate --file=$OUTDIR/spdk-$VER.tar $OUTDIR/spdk-$MOD-$VER.tar 
 done
-gzip $OUTDIR/spdk-$VER.tar
+gzip -c $OUTDIR/spdk-$VER.tar >$OUTDIR/spdk-$VER.tar.gz
 # BUILD_NUMBER is an env var passed by Jenkins
 # https://stackoverflow.com/questions/16155792/using-jenkins-build-number-in-rpm-spec-file
 fakeroot  \
   rpmbuild -bs --define "dist %{nil}" $args $OUT_SPEC
 
-# rpmbuild -bb $args  $OUT_SPEC
+#rpmbuild -bb $args $OUT_SPEC
