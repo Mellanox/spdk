303a330b859f9eb0c5b201745d76cd5072a01e8b
Shuhei Matsumoto <smatsumoto@nvidia.com>
2023-06-08 10:34:46 +0900

    bdev/nvme: Add retry_count check to bdev_nvme_io_complete()
    We should be able to disable retry also for bdev_nvme_io_complete().
    
    Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -1955,24 +1955,25 @@ bdev_nvme_io_complete(struct nvme_bdev_io *bio, int rc)
 		io_status = SPDK_BDEV_IO_STATUS_NOMEM;
 		break;
 	case -ENXIO:
-		nbdev_ch = spdk_io_channel_get_ctx(spdk_bdev_io_get_io_channel(bdev_io));
+		if (g_opts.bdev_retry_count == -1 || bio->retry_count < g_opts.bdev_retry_count) {
+			nbdev_ch = spdk_io_channel_get_ctx(spdk_bdev_io_get_io_channel(bdev_io));
 
-		bdev_nvme_clear_current_io_path(nbdev_ch);
-		bio->io_path = NULL;
+			bdev_nvme_clear_current_io_path(nbdev_ch);
+			bio->io_path = NULL;
 
-		if (spdk_unlikely(STAILQ_EMPTY(&nbdev_ch->io_path_list))) {
-			/* Special case for async lazy bdev connection - IO might come
-			 * when no qpair is created, io_path_list is empty. We should queue
-			 * this IO for retry */
-			bdev_nvme_queue_retry_io(nbdev_ch, bio, 1000ULL);
-			return;
+			if (spdk_unlikely(STAILQ_EMPTY(&nbdev_ch->io_path_list))) {
+				/* Special case for async lazy bdev connection - IO might come
+				 * when no qpair is created, io_path_list is empty. We should queue
+				 * this IO for retry */
+				bdev_nvme_queue_retry_io(nbdev_ch, bio, 1000ULL);
+				return;
+			}
+
+			if (any_io_path_may_become_available(nbdev_ch)) {
+				bdev_nvme_queue_retry_io(nbdev_ch, bio, 1000ULL);
+				return;
+			}
 		}
-
-		if (any_io_path_may_become_available(nbdev_ch)) {
-			bdev_nvme_queue_retry_io(nbdev_ch, bio, 1000ULL);
-			return;
-		}
-
 	/* fallthrough */
 	default:
 		io_status = SPDK_BDEV_IO_STATUS_FAILED;
