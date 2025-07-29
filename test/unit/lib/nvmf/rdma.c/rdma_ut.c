65901b952289e6378d05d3b146daa1304c5336c0
Aleksey Marchuk <alexeymar@nvidia.com>
2025-07-29 09:55:55 +0000

    nvmf/rdma: Move data WR alloc to a new state
    Preparation to support wait-for-buffer queueing using
    iobuf - data WR allocation needs to be handled separately
    from the data buffers, move related code to the new state
    
    Change-Id: Ia8cefda10db89af4e59a389407a068851dec09db
    Signed-off-by: Aleksey Marchuk <alexeymar@nvidia.com>

--- test/unit/lib/nvmf/rdma.c/rdma_ut.c
+++ test/unit/lib/nvmf/rdma.c/rdma_ut.c
@@ -185,6 +185,7 @@ reset_nvmf_rdma_request(struct spdk_nvmf_rdma_request *rdma_req)
 		free(rdma_req->req.stripped_data);
 		rdma_req->req.stripped_data = NULL;
 	}
+	nvmf_rdma_setup_request(rdma_req);
 }
 
 static void
@@ -345,6 +346,8 @@ test_spdk_nvmf_rdma_request_parse_sgl(void)
 		sgl_desc[i].keyed.key = 0x44;
 	}
 	rdma_req.num_wrs = 2;
+	rc = nvmf_request_alloc_wrs(&rtransport, &rdma_req, rdma_req.num_wrs - 1);
+	CU_ASSERT(rc == 0);
 
 	rc = nvmf_rdma_request_parse_sgl(&rtransport, &device, &rdma_req);
 
@@ -371,6 +374,8 @@ test_spdk_nvmf_rdma_request_parse_sgl(void)
 		sgl_desc[i].keyed.key = 0x44;
 	}
 	rdma_req.num_wrs = 2;
+	rc = nvmf_request_alloc_wrs(&rtransport, &rdma_req, rdma_req.num_wrs - 1);
+	CU_ASSERT(rc == 0);
 
 	rc = nvmf_rdma_request_parse_sgl(&rtransport, &device, &rdma_req);
 
@@ -403,6 +408,8 @@ test_spdk_nvmf_rdma_request_parse_sgl(void)
 			      rtransport.transport.opts.io_unit_size / 2;
 
 	rdma_req.num_wrs = 2;
+	rc = nvmf_request_alloc_wrs(&rtransport, &rdma_req, rdma_req.num_wrs - 1);
+	CU_ASSERT(rc == 0);
 
 	rc = nvmf_rdma_request_parse_sgl(&rtransport, &device, &rdma_req);
 
@@ -436,6 +443,9 @@ test_spdk_nvmf_rdma_request_parse_sgl(void)
 	}
 	rdma_req.num_wrs = 2;
 
+	rdma_req.num_wrs = 2;
+	rc = nvmf_request_alloc_wrs(&rtransport, &rdma_req, rdma_req.num_wrs - 1);
+	CU_ASSERT(rc == 0);
 	rc = nvmf_rdma_request_parse_sgl(&rtransport, &device, &rdma_req);
 
 	CU_ASSERT(rc == 0);
@@ -1202,6 +1212,9 @@ test_spdk_nvmf_rdma_request_parse_sgl_with_md(void)
 	rdma_req.req.dif_enabled = true;
 	rtransport.transport.opts.io_unit_size = data_bs * 16;
 	sgl->keyed.length = data_bs * 16;
+	rdma_req.num_wrs = 2;
+	rc = nvmf_request_alloc_wrs(&rtransport, &rdma_req, rdma_req.num_wrs - 1);
+	CU_ASSERT(rc == 0);
 
 	rdma_req.num_wrs = 2;
 
@@ -1296,6 +1309,9 @@ test_spdk_nvmf_rdma_request_parse_sgl_with_md(void)
 		sgl_desc[i].address = 0x4000 + i * data_bs * 4;
 		sgl_desc[i].keyed.key = 0x44;
 	}
+	rdma_req.num_wrs = 2;
+	rc = nvmf_request_alloc_wrs(&rtransport, &rdma_req, rdma_req.num_wrs - 1);
+	CU_ASSERT(rc == 0);
 
 	rdma_req.num_wrs = 2;
 
